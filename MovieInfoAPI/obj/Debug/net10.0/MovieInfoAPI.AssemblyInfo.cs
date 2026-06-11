66ff5cc991def928d05d3930de48e967c4a2554e
Suprabhat Maiti <maitisuprabhat@gmail.com>
2026-06-11 15:58:19 +0530

    2nd commit

--- MovieInfoAPI/obj/Debug/net10.0/MovieInfoAPI.AssemblyInfo.cs
+++ MovieInfoAPI/obj/Debug/net10.0/MovieInfoAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieInfoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+745aae1f01adeddb263574eb93786514d17f06a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieInfoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieInfoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
